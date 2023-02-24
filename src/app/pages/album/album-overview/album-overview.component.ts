ac6eca8585477d1adef455c2e9371e6e0dd92a5c
Velid Imiroviæ <vimsirovic1@etf.unsa.ba>
2023-02-24 10:24:01 +0100

    Created album-overview component

---
+++ src/app/pages/album/album-overview/album-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-album-overview',
+  templateUrl: './album-overview.component.html',
+  styleUrls: ['./album-overview.component.scss']
+})
+export class AlbumOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
