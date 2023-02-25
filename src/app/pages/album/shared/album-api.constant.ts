c3dcd4049580489b0de9fa27530b5b75a91a7a71
Velid Imiroviæ <vimsirovic1@etf.unsa.ba>
2023-02-25 19:03:27 +0100

    Created album model, service and api constants

---
+++ src/app/pages/album/shared/album-api.constant.ts
@@ -0,0 +1,9 @@
+export class AlbumApi {
+    public static GET_ALBUMS = '/sdrbe/album';
+    public static GET_ALL_PERSON_LOVS = '/sdrbe/person/lov';
+    public static SEARCH_ALBUMS = '/sdrbe/album';
+    public static CREATE_ALBUM = '/sdrbe/album';
+    public static UPDATE_ALBUM = '/sdrbe/album';
+    public static DELETE_ALBUM = '/sdrbe/album/#';
+    public static GET_ALBUM = '/sdrbe/album/#';
+}
