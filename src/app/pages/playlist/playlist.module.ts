11d5be3e83d3e6ca0fdbc3828d8cec38aa6f7501
Armin Petrović <apetrovic1@etf.unsa.ba>
2023-03-02 00:57:57 +0100

    Added generate-playlist functionality on the frontend

--- src/app/pages/playlist/playlist.module.ts
+++ src/app/pages/playlist/playlist.module.ts
@@ -4,12 +4,14 @@ import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
 import { PlaylistService } from './shared/playlist.service';
 import { PlaylistSearchComponent } from './playlist-search/playlist-search.component';
 import { AutocompleteLibModule } from 'angular-ng-autocomplete';
+import { GeneratePlaylistComponent } from './generate-playlist/generate-playlist.component';
 
 
 
 @NgModule({
   declarations: [
-    PlaylistSearchComponent
+    PlaylistSearchComponent,
+    GeneratePlaylistComponent
   ],
   imports: [
     SharedModule,
@@ -17,7 +19,8 @@ import { AutocompleteLibModule } from 'angular-ng-autocomplete';
     AutocompleteLibModule
   ],
   exports: [
-    PlaylistSearchComponent
+    PlaylistSearchComponent,
+    GeneratePlaylistComponent
   ], providers: [
     PlaylistService,
     { provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -15,6 +15,7 @@ import { AlbumOverviewComponent } from './pages/album/album-overview/album-overv
 
 import { SongOverviewComponent } from './pages/song/song-overview/song-overview.component';
 import { AlbumCreateComponent } from './pages/album/album-create/album-create.component';
+import { GeneratePlaylistComponent } from './pages/playlist/generate-playlist/generate-playlist.component';
 
 export const appRoutes: Routes = [
   { path: '', component: HomePageComponent },
@@ -37,4 +38,5 @@ export const appRoutes: Routes = [
   { path: 'album/:id/overview', component: AlbumOverviewComponent },
   { path: 'album/create', component: AlbumCreateComponent },
   { path: 'album/update/:id', component: AlbumCreateComponent },
+  { path: 'playlist/generate-playlist', component: GeneratePlaylistComponent }
 ];
