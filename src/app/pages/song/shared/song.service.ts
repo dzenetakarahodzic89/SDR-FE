459d2a84df87cb3b4920240caa984b8d1cc93764
Mahir Prcanović <mahirprcanovic7@gmail.com>
2023-03-06 01:06:25 +0100

    feat: song upload done.

--- src/app/pages/song/shared/song.service.ts
+++ src/app/pages/song/shared/song.service.ts
@@ -3,14 +3,19 @@ import { ZxApi } from '@zff/zx-core';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { SongApi } from './song-api.constant';
-import { AlbumResponse, ArtistResponse, GenreResponse, SongResponse } from './song.model';
-
+import {
+  AlbumResponse,
+  ArtistResponse,
+  FileUploadSegmentCreateRequest,
+  GenreResponse,
+  SongResponse,
+} from './song.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SongService {
-  constructor(private api: ZxApi) { }
+  constructor(private api: ZxApi) {}
   getSong(id: number) {
     return this.api.get(SongApi.GET_SONG.replace('#', id.toString())).pipe(
       map((response) => {
@@ -26,7 +31,7 @@ export class SongService {
         const message = response['payload'];
         return message;
       })
-    )
+    );
   }
 
   getAllGenres(): Observable<GenreResponse[]> {
@@ -35,7 +40,7 @@ export class SongService {
         const stories = response['payload'] as GenreResponse[];
         return stories;
       })
-    )
+    );
   }
 
   getAllAlbums(): Observable<AlbumResponse[]> {
@@ -44,7 +49,7 @@ export class SongService {
         const stories = response['payload'] as AlbumResponse[];
         return stories;
       })
-    )
+    );
   }
 
   getAllArtists(): Observable<ArtistResponse[]> {
@@ -53,6 +58,38 @@ export class SongService {
         const stories = response['payload'] as ArtistResponse[];
         return stories;
       })
-    )
+    );
+  }
+  uploadSong(request: FileUploadSegmentCreateRequest) {
+    return this.api
+      .post(SongApi.UPLOAD_SONG, request)
+      .pipe(
+        map((response) => {
+          return response['payload'];
+        })
+      )
+      .toPromise();
+  }
+  getStatusOfAudio(mediaObjectId: number, type: string): Observable<string> {
+    console.log(
+      SongApi.GET_STATUS.replace('#', mediaObjectId.toString()).replace(
+        '?',
+        type.toString()
+      )
+    );
+    return this.api
+      .get(
+        SongApi.GET_STATUS.replace('#', mediaObjectId.toString()).replace(
+          '?',
+          type.toString()
+        )
+      )
+      .pipe(
+        map((response) => {
+          let message = response['payload'];
+          if (message == 'READY_TO_MERGE') return 'Audio is being processed.';
+          return message;
+        })
+      );
   }
 }
