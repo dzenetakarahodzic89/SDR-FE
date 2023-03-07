da82fd59fd1df67774f1706302f2591f5dd15ebe
Alem <alem.981@outlook.com>
2023-03-07 14:25:42 +0100

    Tabs icons

--- src/app/pages/person/person-overview/person-overview.component.ts
+++ src/app/pages/person/person-overview/person-overview.component.ts
@@ -70,13 +70,13 @@ export class PersonOverviewComponent implements OnInit {
         name: 'Artists',
         id: 'artistsTab',
         label: 'Artists',
-        icon: 'fal fa-persons',
+        icon: 'fal fa-users',
       },
       {
         name: 'Connected Medias',
         id: 'connectedMediasTab',
         label: 'Connected Medias',
-        icon: 'fal fa-film'
+        icon: 'fal fa-check-circle',
       },
     ],
   });
@@ -156,7 +156,7 @@ export class PersonOverviewComponent implements OnInit {
     ]
   });
 
-  createArtistFromPerson():void {
+  createArtistFromPerson(): void {
     this.personService.createArtistFromPerson(this.person.id).subscribe(payload => {
       console.log(payload);
       this.toastr.success("Artist '" + payload.name + " " + payload.surname + "' created!");
